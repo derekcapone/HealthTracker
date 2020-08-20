4efa38627cffd9aea33badeb936da491c7f0a53f
Derek Capone <derekcapone7@gmail.com>
2020-08-19 22:25:41 -0300

    Adds functioning json reader

--- route_handler.py
+++ route_handler.py
@@ -1,27 +1,19 @@
 from datetime import datetime
+import json
+
 
 def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
-# Data to serve with our API
-PEOPLE = {
-    "Farrell": {
-        "fname": "Doug",
-        "lname": "Farrell",
-        "timestamp": get_timestamp()
-    },
-    "Brockman": {
-        "fname": "Kent",
-        "lname": "Brockman",
-        "timestamp": get_timestamp()
-    },
-    "Easter": {
-        "fname": "Bunny",
-        "lname": "Easter",
-        "timestamp": get_timestamp()
-    }
-}
+def get_people():
+    """
+    Reads json file
+    :return: dict of json file
+    """
+    f = open("people.json")
+    file_text = f.read()
+    return json.loads(file_text)
 
 
 # Create a handler for our read (GET) people
@@ -33,4 +25,10 @@ def read():
     :return:        sorted list of people
     """
     # Create the list of people from our data
-    return PEOPLE
+    people_dict = get_people()
+    return people_dict
+
+
+if __name__ == "__main__":
+    people_d = read()
+    print(people_d)
