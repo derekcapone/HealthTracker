be3e0115d7f76ddfa1d0da9c77f453387ea2d395
Derek Capone <derekcapone7@gmail.com>
2020-08-22 21:26:28 -0300

    Implements functionality to connect to database

---
+++ MongoInterface.py
@@ -0,0 +1,53 @@
+import pymongo
+import json
+
+
+def get_private_value(val_name):
+    """
+    Gets a value from the private
+    :param val_name: name of private value to retrieve
+    :return: string containing private value
+    """
+    f = open("private_values.json")
+    priv_str = f.read()
+    f.close()
+    ret_dict = json.loads(priv_str)
+    return ret_dict[val_name]
+
+
+def get_mongo_pw():
+    """
+    Gets MongoDB password from private values
+    :return: password string for MongoDB
+    """
+    return get_private_value("mongo_pw")
+
+
+def connect_to_database():
+    """
+    Connects to database
+    :return: Database object
+    """
+    pw = get_private_value("mongo_pw")
+    db_name = get_private_value("db_name")
+    conn_str = "mongodb+srv://derekcapone7:{}@restful-db-fugmi.mongodb.net/{}?retryWrites=true&w=majority".format(pw, db_name)
+    client = pymongo.MongoClient(conn_str)
+    return client[db_name]
+
+
+def connect_to_collection(col_name):
+    """
+    Connect to desired collection within database
+    :param col_name: name of the collection
+    :return: collection object
+    """
+    db = connect_to_database()
+
+    col_name = get_private_value("collection")
+    return db[col_name]
+
+if __name__ == "__main__":
+    col = connect_to_collection("PeopleData")
+
+    mydict = {"name": "John", "address": "Highway 37"}
+    col.insert_one(mydict)
